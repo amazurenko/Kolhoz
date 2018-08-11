54daf4b8e62d6ad86d1984759979bddbd1794241
Aleksandr Mazurenko <neo-katso@yandex.ru>
2018-08-11 12:32:37 +0300

    removing strict

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-'use strict';
 const path = require('path');
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
